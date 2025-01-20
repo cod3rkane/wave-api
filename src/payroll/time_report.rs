47c73a68e30ca78edcef7f1569086c870925e01c
Cod3rkane <lopes.coder@gmail.com>
2025-01-20 18:38:41 -0500

    Json response example

--- src/payroll/time_report.rs
+++ src/payroll/time_report.rs
@@ -1,3 +1,5 @@
+use rocket::{http::Status, serde::json::Json};
+
 #[put("/payroll/time-report/<report_id>")]
 pub fn time_report(report_id: String) -> &'static str {
     println!("Report ID: {:?}", report_id);
@@ -6,6 +8,6 @@ pub fn time_report(report_id: String) -> &'static str {
 }
 
 #[get("/payroll/time-reports")]
-pub fn time_reports() -> &'static str {
-    "Via Carai!"
+pub fn time_reports() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello Json Response!")))
 }
